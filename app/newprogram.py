10cb81a263dbd059de1737927fd2f87993c989d7
kobayashi porcelain <sinsinackrst@gmail.com>
2023-04-25 13:37:47 +0200

    Add uninstall.sh file to program folder and hash bang for bash

--- app/newprogram.py
+++ app/newprogram.py
@@ -19,9 +19,14 @@ def main():
     # create configs folder
     os.mkdir(f'{new_program_path}/configs')
     # create config file in configs
-    open(f'{new_program_path}/configs/config.sh', 'w')
+    with open(f'{new_program_path}/configs/config.sh', 'w') as config_file:
+        config_file.write("#!/bin/bash")
     # create install file
-    open(f'{new_program_path}/install.sh', 'w')
+    with open(f'{new_program_path}/install.sh', 'w') as install_file:
+        install_file.write("#!/bin/bash")
+    # create uninstall file
+    with open(f'{new_program_path}/uninstall.sh', 'w') as uninstall_file:
+        uninstall_file.write("#!/bin/bash")
 
 
 if __name__ == '__main__':
