c030c2a646d29e574c84c0abc9ff555f41892753
mvalizad <masoud.valizadeh@servizist.it>
2023-04-26 17:11:52 +0200

    Use ProvisionCondigReader class in main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 from builder import get_project_class
+from provisionsreader import ProvisionConfigReader
+import sys
+import logging
+import os
+
+
+sys.tracebacklimit = 0
+vmbuilder_path = f'{os.path.dirname(os.path.realpath(__file__))}/..'
 
 
 def main():
     project = get_project_class()
     project.check_flags()
     project.check_folder_vb_existence()
+    provisions_configs_reader = ProvisionConfigReader(f'{project.provisions_configs}/{project.arguments["-j"]}')
+    provisions_configs_reader.check_programs_existence()
+    provisions_configs_reader.check_scripts_existence()
 
     try:
         project.create_project_folder()
         project.provision()
-    except (FileNotFoundError, KeyError):
+    except (FileNotFoundError, KeyError) as err:
+        logging.error(err)
         project.delete_project()
 
 
