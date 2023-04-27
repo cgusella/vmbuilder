9c4f183b5199c9fb0b7a21e37be3cc2e7d898c58
kobayashi <sinsinackrst@gmail.com>
2023-04-27 15:02:27 +0200

    Create several programs calling the method

--- app/newprogram.py
+++ app/newprogram.py
@@ -6,29 +6,33 @@ import os
 vmbuilder_path = f'{os.path.dirname(os.path.realpath(__file__))}/..'
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-p', '--program-name', dest='program', required=True,
-        help='define new program name to add in template/programs'
-    )
-    arguments = parser.parse_args()
+def make_program_folder(programs: list = []):
+    if not programs:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '-p', '--program-name', dest='programs', required=True,
+            help='define new program name to add in template/programs',
+            nargs='+'
+        )
+        arguments = parser.parse_args()
+        programs = arguments.programs
 
-    new_program_path = f'{vmbuilder_path}/templates/programs/{arguments.program}'
-    # create program folder
-    os.mkdir(new_program_path)
-    # create configs folder
-    os.mkdir(f'{new_program_path}/configs')
-    # create config file in configs
-    with open(f'{new_program_path}/configs/config.sh', 'w') as config_file:
-        config_file.write("#!/bin/bash")
-    # create install file
-    with open(f'{new_program_path}/install.sh', 'w') as install_file:
-        install_file.write("#!/bin/bash")
-    # create uninstall file
-    with open(f'{new_program_path}/uninstall.sh', 'w') as uninstall_file:
-        uninstall_file.write("#!/bin/bash")
+    for program in programs:
+        new_program_path = f'{vmbuilder_path}/templates/programs/{program}'
+        # create program folder
+        os.mkdir(new_program_path)
+        # create configs folder
+        os.mkdir(f'{new_program_path}/configs')
+        # create config file in configs
+        with open(f'{new_program_path}/configs/config.sh', 'w') as config_file:
+            config_file.write("#!/bin/bash")
+        # create install file
+        with open(f'{new_program_path}/install.sh', 'w') as install_file:
+            install_file.write("#!/bin/bash")
+        # create uninstall file
+        with open(f'{new_program_path}/uninstall.sh', 'w') as uninstall_file:
+            uninstall_file.write("#!/bin/bash")
 
 
 if __name__ == '__main__':
-    main()
+    make_program_folder()
