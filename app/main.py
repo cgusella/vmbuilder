b903fbcc186a346d770853e318e8dd5c2c823b24
kobayashi <sinsinackrst@gmail.com>
2023-04-27 15:01:46 +0200

    Use new methods in main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@
 from builder import get_project_class
 from provisionsreader import ProvisionConfigReader
 import sys
-import logging
 import os
-
 
 sys.tracebacklimit = 0
 vmbuilder_path = f'{os.path.dirname(os.path.realpath(__file__))}/..'
@@ -13,16 +11,17 @@ vmbuilder_path = f'{os.path.dirname(os.path.realpath(__file__))}/..'
 def main():
     project = get_project_class()
     project.check_flags()
-    project.check_folder_vb_existence()
+    project.check_folder_vb_json_existence()
     provisions_configs_reader = ProvisionConfigReader(f'{project.provisions_configs}/{project.arguments["-j"]}')
     provisions_configs_reader.check_programs_existence()
     provisions_configs_reader.check_scripts_existence()
+    provisions_configs_reader.check_install_scripts_emptyness()
+    provisions_configs_reader.check_uninstall_scripts_emptyness()
 
     try:
         project.create_project_folder()
         project.provision()
-    except (FileNotFoundError, KeyError) as error:
-        logging.error(error)
+    except (FileNotFoundError, KeyError):
         project.delete_project()
 
 
