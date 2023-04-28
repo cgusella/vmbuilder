a5f480cebeae741cb43076e46e9924dfad48bcaa
cgusella <cristian.gusella@servizist.it>
2023-04-28 14:26:56 +0200

    Add set_configs and set_provisions method to main

--- app/main.py
+++ app/main.py
@@ -3,24 +3,26 @@ from builder import get_project_class
 from provisionsreader import ProvisionConfigReader
 import sys
 
-sys.tracebacklimit = 0
+# sys.tracebacklimit = 0
 
 
 def main():
-    project = get_project_class()
-    project.check_flags()
-    project.check_folder_vb_json_existence()
-    provisions_configs_reader = ProvisionConfigReader(f'{project.provisions_configs}/{project.arguments["-j"]}')
+    builder = get_project_class()
+    builder.check_flags()
+    builder.check_folder_vb_json_existence()
+    builder.set_configs()
+    builder.set_provisions()
+    provisions_configs_reader = ProvisionConfigReader(f'{builder.provisions_configs}/{builder.arguments["-j"]}')
     provisions_configs_reader.check_programs_existence()
     provisions_configs_reader.check_scripts_existence()
     provisions_configs_reader.check_install_scripts_emptyness()
     provisions_configs_reader.check_uninstall_scripts_emptyness()
 
     try:
-        project.create_project_folder()
-        project.provision()
-    except (KeyError):
-        project.delete_project()
+        builder.create_project_folder()
+        builder.provision()
+    except (FileNotFoundError, KeyError):
+        builder.delete_project()
 
 
 if __name__ == '__main__':
