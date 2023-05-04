fb67113c9e808f82b6fd728b73340655b07e0ba3
shonc23 <cristiangus8@gmail.com>
2023-05-04 10:26:08 +0200

    Add test for initial main message

--- app/test/vagranttest.py
+++ app/test/vagranttest.py
@@ -4,9 +4,7 @@ To launch these tests, add first
 $export PYTHONPATH="${PYTHONPATH}:/path-to-vmbuilder/
 """
 
-import subprocess
 import unittest
-from app.constants import vmbuilder_path
 from helpertest import missing_flag_error_test
 
 VAGRANT_FLAGS = {"-n": "name", "-vm": "vboxname", "-t": "vagrant",
@@ -18,6 +16,72 @@ class VagrantCheckFlags(unittest.TestCase):
 
     def setUp(self) -> None:
         self.vmtype = 'vagrant'
+
+    def test_no_flag_at_all(self):
+        """Test correct info are printed when no
+        flags are given
+        """
+        error_msg = missing_flag_error_test(
+            flags=["-n", "-vm", "-t", "-u", "-ho", "-i",
+                   "-j", "-s"],
+            vmtype=self.vmtype
+        )
+        self.assertIn(
+            '-n\t[PROJECT NAME]',
+            error_msg
+        )
+        self.assertIn(
+            '-vm\t[VBOXNAME]',
+            error_msg
+        )
+        self.assertIn(
+            '-t\tvagrant',
+            error_msg
+        )
+        self.assertIn(
+            '-u:\t\t[EXTRA SUDOER USER]',
+            error_msg
+        )
+        self.assertIn(
+            '-ho:\t\t[HOSTNAME]',
+            error_msg
+        )
+        self.assertIn(
+            '-i:\t\t[VAGRANT IMAGE]',
+            error_msg
+        )
+        self.assertIn(
+            '-j:\t\t[VAGRANT PROVISION FILE]',
+            error_msg
+        )
+        self.assertIn(
+            '-s:\t\t[VAGRANT SSH CONNECTION TYPE]',
+            error_msg
+        )
+        self.assertIn(
+            '-t\tpacker',
+            error_msg
+        )
+        self.assertIn(
+            '-il:\t\t[ISO LINK]',
+            error_msg
+        )
+        self.assertIn(
+            '-if:\t\t[ISO FILE]',
+            error_msg
+        )
+        self.assertIn(
+            '-cs:\t\t[CHECKSUM]',
+            error_msg
+        )
+        self.assertIn(
+            '-j:\t\t[PACKER CONFIG FILE]',
+            error_msg
+        )
+        self.assertIn(
+            '-pf:\t\t[PRESEED FILE]',
+            error_msg
+        )
 
     def test_no_flag_name(self):
         """Test if error message is printed when "-n"
