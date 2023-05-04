2d2e1ac2c9a7b2483aa1c7384cd3eb79d5718be9
shonc23 <cristiangus8@gmail.com>
2023-05-04 10:44:16 +0200

    End vagrant check_flags method testing

--- app/test/vagranttest.py
+++ app/test/vagranttest.py
@@ -3,8 +3,9 @@ Test file for Vagrant.
 To launch these tests, add first
 $export PYTHONPATH="${PYTHONPATH}:/path-to-vmbuilder/
 """
-
+import os
 import unittest
+from app import constants
 from helpertest import missing_flag_error_test
 
 VAGRANT_FLAGS = {"-n": "name", "-vm": "vboxname", "-t": "vagrant",
@@ -92,8 +93,8 @@ class VagrantCheckFlags(unittest.TestCase):
             vmtype=self.vmtype
         )
         self.assertEqual(
-            error_msg,
-            'error.FlagError: \n\t-n\t[PROJECT NAME]\n\n'
+            'error.FlagError: \n\t-n\t[PROJECT NAME]\n\n',
+            error_msg
         )
 
     def test_no_flag_box(self):
@@ -105,8 +106,8 @@ class VagrantCheckFlags(unittest.TestCase):
             vmtype=self.vmtype
         )
         self.assertEqual(
-            error_msg,
-            'error.FlagError: \n\t-vm\t[VBOXNAME]\n\n'
+            'error.FlagError: \n\t-vm\t[VBOXNAME]\n\n',
+            error_msg
         )
 
     def test_no_flag_type(self):
@@ -118,8 +119,82 @@ class VagrantCheckFlags(unittest.TestCase):
             vmtype=self.vmtype
         )
         self.assertEqual(
+            'error.FlagError: \n\t-t\t[vagrant|packer]\n\n',
+            error_msg
+        )
+
+    def test_no_flag_user(self):
+        """Test if error message is printed when "-u"
+        flag is not specified
+        """
+        error_msg = missing_flag_error_test(
+            flags=["-u"],
+            vmtype=self.vmtype
+        )
+        self.assertIn(
+            '-u:\t[EXTRA SUDOER USER]',
             error_msg,
-            'error.FlagError: \n\t-t\t[vagrant|packer]\n\n'
+        )
+
+    def test_no_flag_hostname(self):
+        """Test if error message is printed when "-ho"
+        flag is not specified
+        """
+        error_msg = missing_flag_error_test(
+            flags=["-ho"],
+            vmtype=self.vmtype
+        )
+        self.assertIn(
+            '-ho:\t[HOSTNAME]',
+            error_msg,
+        )
+
+    def test_no_flag_image(self):
+        """Test if error message is printed when "-i"
+        flag is not specified
+        """
+        error_msg = missing_flag_error_test(
+            flags=["-i"],
+            vmtype=self.vmtype
+        )
+        self.assertIn(
+            '-i:\t[VAGRANT IMAGE]',
+            error_msg,
+        )
+
+    def test_no_flag_provision_file(self):
+        """Test if error message is printed when "-j"
+        flag is not specified
+        """
+        error_msg = missing_flag_error_test(
+            flags=["-j"],
+            vmtype=self.vmtype
+        )
+        self.assertIn(
+            '-j:\t[VAGRANT PROVISION FILE]',
+            error_msg,
+        )
+        for provision_file in os.listdir(constants.vagrant_provs_confs_path):
+            self.assertIn(
+                provision_file,
+                error_msg
+            )
+
+    def test_no_flag_connection(self):
+        """Test if error message is printed when "-s"
+        flag is not specified
+        """
+        error_msg = missing_flag_error_test(
+            flags=["-s"],
+            vmtype=self.vmtype
+        )
+        self.assertIn(
+            '-s:\t[VAGRANT SSH CONNECTION TYPE]',
+            error_msg,
+        )
+        self.assertIn(
+            'password|key',
+            error_msg,
         )
 
 
