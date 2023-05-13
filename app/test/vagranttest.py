a04c897c6f54db1e17ed7134c090aecb0916ca7b
crisgus <86733390+crisgus@users.noreply.github.com>
2023-05-13 14:36:33 +0200

    Restart test for vagrant; add check existence folder test

--- app/test/vagranttest.py
+++ app/test/vagranttest.py
@@ -1,263 +1,50 @@
 """
 Test file for Vagrant.
 To launch these tests, add first
-$export PYTHONPATH="${PYTHONPATH}:/path-to-vmbuilder/
+$export PYTHONPATH="${PYTHONPATH}:/path-to-app/
 """
+import constants
+import os
 import random
 import string
-import os
 import unittest
-from app import constants
-from helpertest import (
-    missing_flag_error_test,
-    launch_main_with_custom_arguments
-)
-
-VAGRANT_FLAGS = {"-n": "name", "-vm": "vboxname", "-t": "vagrant",
-                 "-u": "user", "-ho": "hostname", "-i": "image",
-                 "-j": "provision-file.json", "-s": "key"}
-
-VMTYPE = 'vagrant'
+from argparse import Namespace
+from argumentparser.customparser import CustomArgumentParser
+from builder.error import ExistenceProjectError
+from builder.vagrant import Vagrant
 
 
-class VagrantCheckFlags(unittest.TestCase):
-
-    def test_no_flag_at_all(self):
-        """Test correct info are printed when no
-        flags are given
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-n", "-vm", "-t", "-u", "-ho", "-i",
-                   "-j", "-s"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-n\t[PROJECT NAME]',
-            error_msg
-        )
-        self.assertIn(
-            '-vm\t[VBOXNAME]',
-            error_msg
-        )
-        self.assertIn(
-            '-t\tvagrant',
-            error_msg
-        )
-        self.assertIn(
-            '-u:\t\t[EXTRA SUDOER USER]',
-            error_msg
-        )
-        self.assertIn(
-            '-ho:\t\t[HOSTNAME]',
-            error_msg
-        )
-        self.assertIn(
-            '-i:\t\t[VAGRANT IMAGE]',
-            error_msg
-        )
-        self.assertIn(
-            '-j:\t\t[VAGRANT PROVISION FILE]',
-            error_msg
-        )
-        self.assertIn(
-            '-s:\t\t[VAGRANT SSH CONNECTION TYPE]',
-            error_msg
-        )
-        self.assertIn(
-            '-t\tpacker',
-            error_msg
-        )
-        self.assertIn(
-            '-il:\t\t[ISO LINK]',
-            error_msg
-        )
-        self.assertIn(
-            '-if:\t\t[ISO FILE]',
-            error_msg
-        )
-        self.assertIn(
-            '-cs:\t\t[CHECKSUM]',
-            error_msg
-        )
-        self.assertIn(
-            '-j:\t\t[PACKER CONFIG FILE]',
-            error_msg
-        )
-        self.assertIn(
-            '-pf:\t\t[PRESEED FILE]',
-            error_msg
-        )
-
-    def test_no_flag_name(self):
-        """Test if error message is printed when "-n"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-n"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-n\t[PROJECT NAME]',
-            error_msg
-        )
-
-    def test_no_flag_box(self):
-        """Test if error message is printed when "-vm"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-vm"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-vm\t[VBOXNAME]',
-            error_msg
-        )
-
-    def test_no_flag_type(self):
-        """Test if error message is printed when "-t"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-t"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-t\t[vagrant|packer]',
-            error_msg
-        )
-
-    def test_no_flag_user(self):
-        """Test if error message is printed when "-u"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-u"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-u:\t[EXTRA SUDOER USER]',
-            error_msg,
-        )
-
-    def test_no_flag_hostname(self):
-        """Test if error message is printed when "-ho"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-ho"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-ho:\t[HOSTNAME]',
-            error_msg,
-        )
-
-    def test_no_flag_image(self):
-        """Test if error message is printed when "-i"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-i"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-i:\t[VAGRANT IMAGE]',
-            error_msg,
-        )
-
-    def test_no_flag_provision_file(self):
-        """Test if error message is printed when "-j"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-j"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-j:\t[VAGRANT PROVISION FILE]',
-            error_msg,
-        )
-        provision_files = [
-            file for file in os.listdir(constants.vagrant_provs_confs_path)
-            if file != 'template.json'
-        ]
-        for provision_file in provision_files:
-            self.assertIn(
-                provision_file,
-                error_msg
-            )
-
-    def test_no_flag_connection(self):
-        """Test if error message is printed when "-s"
-        flag is not specified
-        """
-        error_msg = missing_flag_error_test(
-            flags=["-s"],
-            vmtype=VMTYPE
-        )
-        self.assertIn(
-            '-s:\t[VAGRANT SSH CONNECTION TYPE]',
-            error_msg,
-        )
-        self.assertIn(
-            'password|key',
-            error_msg,
-        )
+def generate_name_in_folder(folder_path: str):
+    """Generate and return a string which mock the name
+    of a file or folder that does not exist in the given folder
+    """
+    name_in_folder = False
+    while not name_in_folder:
+        random_file_name = f"""{''.join(
+            random.choices(string.ascii_lowercase, k=5)
+        )}.json"""
+        if random_file_name not in os.listdir(folder_path):
+            name_in_folder = True
+    return random_file_name
 
 
-class VagrantCheckFolderVbJsonExistence(unittest.TestCase):
+class VagrantTest(unittest.TestCase):
 
     def test_check_new_project_folder_existence(self):
-        existing_machines = os.listdir(constants.vagrant_machines_path)
-        if existing_machines:
-            error_msg = launch_main_with_custom_arguments(
-                {"-n": f"{existing_machines[0]}"},
-                vmtype=VMTYPE,
-                error=True
-            )
-            self.assertIn(
-                "ExistenceProjectError",
-                error_msg
-            )
-        else:
-            os.mkdir(f"{constants.vagrant_machines_path}/test")
-            error_msg = launch_main_with_custom_arguments(
-                {"-n": "test"},
-                vmtype=VMTYPE,
-                error=True
-            )
-            self.assertIn(
-                "ExistenceProjectError",
-                error_msg
-            )
-            os.rmdir(f"{constants.vagrant_machines_path}/test")
-
-    def test_check_provision_cfg_json_existence(self):
-        provision_file_in_folder = False
-        while not provision_file_in_folder:
-            new_provision_file_name = f"""{''.join(
-                random.choices(string.ascii_lowercase, k=5)
-            )}.json"""
-            existing_provisions = os.listdir(constants.vagrant_provs_confs_path)
-            if new_provision_file_name not in existing_provisions:
-                provision_file_in_folder = True
-        result = launch_main_with_custom_arguments(
-            {"-j": new_provision_file_name},
-            vmtype=VMTYPE,
-            error=True
+        """
+        Test that an error is raised when a project with a given
+        name already exists
+        """
+        namespace_test = Namespace()
+        project_name = generate_name_in_folder(
+            folder_path=constants.vagrant_machines_path
         )
-        self.assertIn(
-            'JsonConfigCopiedError',
-            result
-        )
-        self.assertIn(
-            new_provision_file_name,
-            os.listdir(constants.vagrant_provs_confs_path)
-        )
-        os.remove(
-            f'{constants.vagrant_provs_confs_path}/{new_provision_file_name}'
-        )
+        namespace_test.name = project_name
+        self.vagrant = Vagrant(namespace_test)
+        os.mkdir(f'{constants.vagrant_machines_path}/{project_name}')
+        with self.assertRaises(ExistenceProjectError):
+            self.vagrant.check_new_project_folder_existence()
+        os.rmdir(f'{constants.vagrant_machines_path}/{project_name}')
 
 
 if __name__ == '__main__':
