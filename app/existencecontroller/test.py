6efa4b19cf183bed644c5560c5c089dbd8357df4
shonc23 <cristiangus8@gmail.com>
2023-05-17 16:00:02 +0200

    Add tests for packer controller class

--- app/existencecontroller/test.py
+++ app/existencecontroller/test.py
@@ -9,7 +9,7 @@ import random
 import string
 import unittest
 from argparse import Namespace
-from existencecontroller.controller import VagrantController
+from existencecontroller.controller import VagrantController, PackerController
 from existencecontroller.errors import (
     FileExtesionError,
     ExistenceProjectError
@@ -75,6 +75,52 @@ class VagrantControllerTest(unittest.TestCase):
         with self.assertRaises(ExistenceProjectError):
             controller.check_new_project_folder_existence()
         os.rmdir(f'{constants.VAGRANT_MACHINES_PATH}/{new_project_name}')
+
+
+class PackerControllerTest(unittest.TestCase):
+
+    def test_check_json_existence(self):
+        """Test that if selected JSON does not exist, then a new JSON
+        file with the given name and a warning is printed in terminal
+        """
+        namespace = Namespace()
+        json_file_name = generate_new_name_in(constants.PACKER_PROVS_CONFS_PATH)
+        namespace.json = json_file_name
+        controller = PackerController(namespace=namespace)
+        try:
+            with self.assertLogs(level='WARNING'):
+                controller.check_json_existence()
+        except SystemExit:
+            pass
+
+        self.assertIn(
+            json_file_name,
+            os.listdir(constants.PACKER_PROVS_CONFS_PATH)
+        )
+        os.remove(f'{constants.PACKER_PROVS_CONFS_PATH}/{json_file_name}')
+
+    def test_json_extension(self):
+        """Test that an error is raised when a JSON file has not the proper
+        json extension
+        """
+        namespace = Namespace()
+        json_file_name = generate_new_name_in(constants.PACKER_PROVS_CONFS_PATH)
+        namespace.json = json_file_name[0:6] + '.js'
+        controller = PackerController(namespace=namespace)
+        with self.assertRaises(FileExtesionError):
+            controller.check_json_existence()
+
+    def test_check_new_project_folder_existence(self):
+        """Test that if you create a project with a used name, then an error
+        is raised"""
+        new_project_name = generate_new_name_in(constants.PACKER_MACHINES_PATH)
+        os.mkdir(f'{constants.PACKER_MACHINES_PATH}/{new_project_name}')
+        namespace = Namespace()
+        namespace.name = new_project_name
+        controller = PackerController(namespace=namespace)
+        with self.assertRaises(ExistenceProjectError):
+            controller.check_new_project_folder_existence()
+        os.rmdir(f'{constants.PACKER_MACHINES_PATH}/{new_project_name}')
 
 
 if __name__ == '__main__':
